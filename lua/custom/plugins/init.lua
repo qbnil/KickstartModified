fb0acdd9f215dca3781d4ba940e36f3ec0f9943d
qbnil <tomederson92@gmail.com>
2024-10-09 08:43:30 +0300

    2 stage of modifications

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+    -- plugins/quarto.lua
+    {
+        'quarto-dev/quarto-nvim',
+        dependencies = {
+            'jmbuhr/otter.nvim',
+            'nvim-treesitter/nvim-treesitter',
+        },
+    },
+}
